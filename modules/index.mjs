cfb34bd1a5457e5e3193454d69cbd7109ec868cf
Arpana <arpana@itobuz.com>
2023-03-03 18:43:09 +0530

    created basic modules

---
+++ modules/index.mjs
@@ -0,0 +1,21 @@
+// const math = require('./math');
+// console.log(math.add(1,3));
+
+
+import {add} from './math.mjs';
+console.log(add(10,2));
+
+import {sub} from './math.mjs';
+console.log(sub(10,2));
+
+import {multi} from './math.mjs';
+console.log(multi(10,2));
+
+import {div} from './math.mjs';
+console.log(div(10,2));
+
+import {per} from './math.mjs';
+console.log(per(10,2));
+
+
+
